80aa2c7b43318970ec7ae7655bac79a17eb6caa9
Fraz Ali <frazaliqureshi@yahoo.com>
2017-12-16 16:48:59 +0100

    d1p2: Finalized solution

--- 2017/d1/p2-inverse-captcha.py
+++ 2017/d1/p2-inverse-captcha.py
@@ -12,14 +12,20 @@ def read_input(filename):
 			if c.isdigit():
 				data.append(int(c))
 
-			print("Read a character: %c" % c)
+			# print("Read a character: %c" % c)
 	return data
 
 def parse_input(data, size):
-    return 0
+    step = size / 2
+    result = 0
+    for i in range(0, size):
+        if data[i] == data[(i + step) % size]:
+            result += data[i]
+
+    return result
 
 if __name__ == '__main__':
-    data = read_input("p1-input.txt")
-    print("data: %i" % len(data))
+    data = read_input("p2-input.txt")
+    # print("data: %i" % len(data))
     result = parse_input(data, len(data))
     print("Result: %i" % result)
