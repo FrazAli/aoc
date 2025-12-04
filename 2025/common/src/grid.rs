d855f0f6243394c5dffb9870ce562701ea52fded
Fraz Ali <frazaliqureshi@gmail.com>
2025-12-04 08:14:03 +0100

    feat(common): adds 3x3 8-neighbour grid direction offsets

---
+++ 2025/common/src/grid.rs
@@ -0,0 +1,11 @@
+/// Direction offsets for 8-way adjacency on a 3x3 grid.
+pub const DIRS_8: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
